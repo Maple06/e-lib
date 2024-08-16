75f179ba118141e4f99b423781fca9dedd9686e3
Maple06 <malendra.sahla.rizky@gmail.com>
2024-08-16 10:49:53 +0700

    CHANGE - Login page
    Changed login page UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,19 +3,70 @@
 @section('title', 'Login')
 
 @section('content')
+<script src="https://accounts.google.com/gsi/client" async></script>
+<style>
+.login-page {
+    background-image: url({{ asset('images/login-bg.jpg') }});
+    backdrop-filter: blur(10px);
+}
+
+.separator {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: gray !important;
+}
+
+.separator::before,
+.separator::after {
+  content: '';
+  flex: 1;
+  border-bottom: 1px solid gray;
+}
+
+.separator:not(:empty)::before {
+  margin-right: .25em;
+}
+
+.separator:not(:empty)::after {
+  margin-left: .25em;
+}
+
+.g-sign-in-button .content-wrapper {
+    min-height: auto !important;
+    margin-left: 0px;
+    background-color: transparent !important;
+}
+
+.input-group .input-group-text {
+    border-left: 1px solid #ced4da !important;
+    border-right: 0 !important;
+    border-radius: 5px 0 0 5px !important;
+}
+
+.input-group .form-control {
+    border-left: 0 !important;
+    border-right: 1px solid #ced4da !important;
+}
+
+button {
+    width: 60% !important;
+    margin: auto;
+}
+</style>
 <div class="card">
     <div class="card-body login-card-body">
-        <p class="login-box-msg">Masuk untuk memulai sesi Anda</p>
+        <p class="login-box-msg">Selamat datang! <br> Masuk untuk memulai sesi Anda</p>
 
         <form action="{{ route('login') }}" method="post">
             @csrf
             <div class="input-group mb-3">
-                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" value="{{ old('email') }}" required autofocus>
                 <div class="input-group-append">
                     <div class="input-group-text">
                         <span class="fas fa-envelope"></span>
                     </div>
                 </div>
+                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" value="{{ old('email') }}" required autofocus>
                 @error('email')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -23,34 +74,48 @@
                 @enderror
             </div>
             <div class="input-group mb-3">
-                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Kata Sandi" required>
                 <div class="input-group-append">
                     <div class="input-group-text">
                         <span class="fas fa-lock"></span>
                     </div>
                 </div>
+                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Kata Sandi" required>
                 @error('password')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
                 @enderror
             </div>
-            <div class="row">
-                <div class="col-8">
-                    <div class="icheck-primary">
-                        <input type="checkbox" name="remember" id="remember">
-                        <label for="remember">
-                            Ingat Saya
-                        </label>
-                    </div>
-                </div>
-                <!-- /.col -->
-                <div class="col-4">
-                    <button type="submit" class="btn btn-primary btn-block">Masuk</button>
-                </div>
-                <!-- /.col -->
+
+            <div class="icheck-primary">
+                <input type="checkbox" name="remember" id="remember">
+                <label for="remember">
+                    Ingat Saya
+                </label>
             </div>
+            <br />
+            <button type="submit" class="btn btn-primary btn-block">Masuk</button>
         </form>
+        <br />
+        <div class="separator">or</div>
+        <br />
+        <div id="g_id_onload"
+            data-client_id="NONE"
+            data-context="signin"
+            data-ux_mode="popup"
+            data-login_uri="NONE"
+            data-auto_prompt="false">
+        </div>
+
+        <div class="g_id_signin"
+            data-type="standard"
+            data-shape="rectangular"
+            data-theme="outline"
+            data-text="signin_with"
+            data-size="large"
+            data-logo_alignment="left">
+        </div>
+        <p>*Login with google will be available later</p>
     </div>
     <!-- /.login-card-body -->
 </div>
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,29 +3,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title') - {{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title') - {{ config('app.name', 'e-Library') }}</title>
     <!-- AdminLTE CSS -->
     <link rel="stylesheet" href="{{ asset('AdminLTE-3.2.0/plugins/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('AdminLTE-3.2.0/dist/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/google.css') }}">
     <!-- Custom CSS -->
     <style>
         body {
             background-color: #f4f6f9;
         }
+        .login-logo b {
+            font-weight: 700 !important;
+        }
+        label {
+            font-weight: normal !important;
+        }
         .login-box {
-            margin-top: 10%;
+            border-radius: 10px;
+            background-color: rgba(255, 255, 255, 0.9);
+        }
+        .login-logo {
+            margin-bottom: 0px;
+            padding: 10px 0px;
+            border-radius: 10px 10px 0px 0px; /*TL TR BR BL*/
+        }
+        .card, .card-body{
+            border-radius: 0 0 10px 10px !important;
+            /* background-color: rgba(244, 245, 216, 0.9); */
         }
     </style>
 </head>
 <body class="hold-transition login-page">
-    <div class="login-box">
-        <div class="login-logo">
-            <a href="{{ url('/') }}"><b>{{ config('app.name', 'Laravel') }}</b></a>
+    <div class="col d-flex justify-content-center align-items-center m-auto w-50">
+        <div class="login-box shadow">
+            <div class="login-logo">
+                <a href="{{ url('/') }}"><b>{{ config('app.name', 'e-Library') }}</b></a>
+            </div>
+            <!-- /.login-logo -->
+            @yield('content')
         </div>
-        <!-- /.login-logo -->
-        @yield('content')
+        <!-- /.login-box -->
+        <div class="details-box">
+            
+        </div>
     </div>
-    <!-- /.login-box -->
 
     <!-- AdminLTE JS -->
     <script src="{{ asset('AdminLTE-3.2.0/plugins/jquery/jquery.min.js') }}"></script>
